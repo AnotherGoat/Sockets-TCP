bf6887f6d5d3b8d526ca9272b1c8499fa4c994f9
Víctor Mardones <v.mardones04@ufromail.cl>
2021-05-07 13:07:27 -0400

    Se añadió verificación del uso

--- src/main/java/MainCliente.java
+++ src/main/java/MainCliente.java
@@ -2,6 +2,11 @@ public class MainCliente {
 
     public static void main(String[] args) {
 
+        if (args.length != 2) {
+            System.out.println("USO: java -jar MainCliente.jar <host> <puerto>");
+            System.exit(0);
+        }
+
         String host = args[0];
         int puerto = Integer.parseInt(args[1]);
 
--- src/main/java/MainServidor.java
+++ src/main/java/MainServidor.java
@@ -2,6 +2,11 @@ public class MainServidor {
 
     public static void main(String[] args) {
 
+        if (args.length != 1) {
+            System.out.println("USO: java -jar MainServidor.jar <puerto>");
+            System.exit(0);
+        }
+
         int puerto = Integer.parseInt(args[0]);
 
         ServidorTCP servidor = new ServidorTCP(puerto);
